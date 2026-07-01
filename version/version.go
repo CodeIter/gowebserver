d57251d17b9a150b762a9af7f168d7eda252735c
Muhammad Amin Boubaker <muhammadaminboubaker@gmail.com>
2026-07-01 15:07:56 +0100

    feat: Add versioning information with Info struct and build-time variables

---
+++ version/version.go
@@ -0,0 +1,33 @@
+package version
+
+import "fmt"
+
+type Info struct {
+	Version   string
+	GitCommit string
+	BuildDate string
+	GoVersion string
+}
+
+// These variables are set via ldflags at build time
+var (
+	version   = "dev"
+	gitCommit = "unknown"
+	buildDate = "unknown"
+	goVersion = "unknown"
+)
+
+// Get returns the populated Info struct
+func Get() Info {
+	return Info{
+		Version:   version,
+		GitCommit: gitCommit,
+		BuildDate: buildDate,
+		GoVersion: goVersion,
+	}
+}
+
+func (i Info) String() string {
+	return fmt.Sprintf("%s (%s) built on %s with %s", 
+		i.Version, i.GitCommit, i.BuildDate, i.GoVersion)
+}
