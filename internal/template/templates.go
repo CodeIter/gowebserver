3edc4dcfde5a4e78979261c79c2cf080de63faba
Muhammad Amin Boubaker <muhammadaminboubaker@gmail.com>
2026-07-06 04:16:58 +0100

    feat: Enhance template loading by collecting HTML files from views directory and add layout and meta partials

--- internal/template/templates.go
+++ internal/template/templates.go
@@ -1,9 +1,12 @@
 package template
 
 import (
+	"fmt"
 	"html/template"
+	"io/fs"
 	"log/slog"
 	"net/http"
+	"path/filepath"
 
 	assets "github.com/CodeIter/gowebserver"
 )
@@ -11,11 +14,28 @@ import (
 // templates holds the parsed HTML templates for rendering views.
 var templates *template.Template
 
-// LoadTemplates parses the HTML templates from the embedded filesystem.
+// LoadTemplates parses all HTML templates from the views directory.
 func LoadTemplates() error {
-	var err error
-	templates, err = template.ParseFS(assets.EmbeddedFiles, "views/*.html")
-	return err
+	var templateFiles []string
+
+	// Walk through the views directory and collect all .html files
+	err := fs.WalkDir(assets.EmbeddedFiles, "views", func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+		if !d.IsDir() && filepath.Ext(path) == ".html" {
+			templateFiles = append(templateFiles, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return fmt.Errorf("error walking templates directory: %w", err)
+	}
+
+	// Parse all collected template files
+	var templateErr error
+	templates, templateErr = template.ParseFS(assets.EmbeddedFiles, templateFiles...)
+	return templateErr
 }
 
 // RenderTemplate renders the specified template with the provided data.
