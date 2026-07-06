6afff7dc1617f2d60887d48c78202f6ff1f9de4a
Muhammad Amin Boubaker <muhammadaminboubaker@gmail.com>
2026-07-06 04:15:04 +0100

    feat: Add getfiles.sh script to list and concatenate file contents, and update .gitignore and .dockerignore to include getfiles.txt

---
+++ getfiles.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+# getfiles.sh
+# This script lists and concatenates the contents of selected files
+# in the current directory and its subdirectories, excluding certain
+# directories.
+# Useful for feeding the contents of files to a language model for analysis
+# or summarization.
+
+tree --gitignore -I 'node_modules|.git|vendor'
+
+for file in "${@}" ; do
+    echo "=================================================================="
+    echo File: "$file"
+    echo "=================================================================="
+    cat "$file"
+    echo "=================================================================="
+done | tee "getfiles.txt"
